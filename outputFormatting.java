0b00047f9cdf0396b0ddf6b3a1f0f2124babab64
Aaron Maraya <aaronmaraya7@gmail.com>
2023-02-01 23:58:08 +0800

    First commit
    -outputFormatting.java file

---
+++ outputFormatting.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class test {
+
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("================================");
+            for(int i = 0; i < 3; i++){
+                String name = input.next();
+                int number = input.nextInt();
+                
+                System.out.printf("%-14s %03d \n", name, x);
+            }
+            System.out.println("================================");
+
+    }
+}
