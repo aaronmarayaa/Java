46ba182307c45fa97ff2b0fd197283b87017ae63
Aaron Maraya <aaronmaraya7@gmail.com>
2023-02-05 18:07:00 +0800

    Name the class same as the file name
    -outputFormatting.java file

--- outputFormatting.java
+++ outputFormatting.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class test {
+public class outputFormatting {
 
     public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
@@ -9,7 +9,7 @@ public class test {
                 String name = input.next();
                 int number = input.nextInt();
                 
-                System.out.printf("%-14s %03d \n", name, x);
+                System.out.printf("%-14s %03d \n", name, number);
             }
             System.out.println("================================");
 
